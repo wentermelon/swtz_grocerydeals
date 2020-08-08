578050b5bdcc57f6e88261522cdc22fe25588839
tshinin <tmshinin@gmail.com>
2020-08-08 15:19:44 -0400

    combined Shamsi and Zaza's work

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/productQuery", methods=['POST'])
+def data():
+    productName = request.form['product']
+    userAddress = request.form['address']
+    return productName + " , " + userAddress
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
