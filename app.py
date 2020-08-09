ee9e994bd16de4450c9ec8dcd15d08eea2be653b
TechyFirestar <soloyyz2000@yahoo.com>
2020-08-09 16:28:34 -0400

    FINAL PUSH-code complete

--- app.py
+++ app.py
@@ -23,12 +23,8 @@ def data():
     productInfoMetro = flask_metro.flask_metro(productName)
     productInfoLongos = flask_longos.flask_longos(productName)
     # productInfoZehrs = flask_zehrs.flask_zehrs(productName)
-<<<<<<< HEAD
-    productInfoNofrills = flask_nofrills.flask_nofrills(productName)
-=======
     # productInfoNofrills = flask_nofrills.flask_nofrills(productName)
 
->>>>>>> 2476d1bce5840e10000c55345e58c2299f01d210
     nearbyStores = flask_googlemapsearch.flask_googlemapsearch(
         userAddress, userRadius)
     # Metro Product Info
@@ -78,13 +74,6 @@ def data():
     #     productNameNofrills.append(key)
     #     productPriceNofrills.append(value[0])
     #     productUnitNofrills.append(value[1])
-
-<<<<<<< HEAD
-    return productInfoNofrills
-
-=======
-    # return productInfoNofrills
->>>>>>> 2476d1bce5840e10000c55345e58c2299f01d210
     #print(productName, file=sys.stdout)
 
     
